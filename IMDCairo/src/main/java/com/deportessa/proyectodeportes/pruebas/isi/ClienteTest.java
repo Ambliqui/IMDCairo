5ebbfdfadf8c9ebe714f89cd376ba656edeabf0a
Ambliqui <isiseren@gmail.com>
2021-10-26 21:43:01 +0200

    Login correcto sin escenarios
    Falta implementar los escenarios de el caso de uso

---
+++ IMDCairo/src/main/java/com/deportessa/proyectodeportes/pruebas/isi/ClienteTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.deportessa.proyectodeportes.pruebas.isi;
+
+import com.deportessa.proyectodeportes.modelo.Cliente;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Mefisto
+ */
+@Local
+public interface ClienteTest {
+    
+    Cliente findEmail(Cliente cliente);
+    
+    Cliente findEmail(String email);
+    
+}
---
+++ IMDCairo/src/main/java/com/deportessa/proyectodeportes/pruebas/isi/ClienteTestImpl.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.deportessa.proyectodeportes.pruebas.isi;
+
+import com.deportessa.proyectodeportes.modelo.Cliente;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+@Stateless
+public class ClienteTestImpl implements ClienteTest {
+
+    @PersistenceContext(unitName = "pu")
+    private EntityManager em;
+
+    @Override
+    public Cliente findEmail(Cliente cliente) {
+        return em.merge(cliente);
+    }
+
+    @Override
+    public Cliente findEmail(String email) {
+
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<Cliente> criteriaQuery = criteriaBuilder.createQuery(Cliente.class);
+        Root root = criteriaQuery.from(Cliente.class);
+        criteriaQuery.where(criteriaBuilder.like(root.get("emailCliente"), email));
+        
+        TypedQuery<Cliente> typedQuery = em.createQuery(criteriaQuery);
+        return typedQuery.getSingleResult();
+        
+    }
+
+}
