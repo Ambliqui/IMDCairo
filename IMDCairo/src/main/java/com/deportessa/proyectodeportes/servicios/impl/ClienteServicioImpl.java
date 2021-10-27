53c713f3054d803c5c7b0efd0f79a1a11d5733bc
Ambliqui <isiseren@gmail.com>
2021-10-27 14:02:49 +0200

    Excepcion de Email
    Revisar Email Exception

--- IMDCairo/src/main/java/com/deportessa/proyectodeportes/servicios/impl/ClienteServicioImpl.java
+++ IMDCairo/src/main/java/com/deportessa/proyectodeportes/servicios/impl/ClienteServicioImpl.java
@@ -5,9 +5,12 @@
  */
 package com.deportessa.proyectodeportes.servicios.impl;
 
+import com.deportessa.proyectodeportes.servicios.Excepciones.EmailNoExistsException;
 import com.deportessa.proyectodeportes.daojpa.ClienteFacadeLocal;
 import com.deportessa.proyectodeportes.modelo.Cliente;
 import com.deportessa.proyectodeportes.servicios.ClienteServicio;
+import java.util.Optional;
+import java.util.function.Supplier;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
@@ -22,6 +25,17 @@ public class ClienteServicioImpl implements ClienteServicio {
         //TODO: Devolver un cliente DTO
 //        cliente = clienteDao.edit(cliente);
         return clienteDao.find(cliente.getIdCliente());
+    }
+
+    @Override
+    public Cliente findEmail(Cliente cliente) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Cliente findEmail(String email) throws EmailNoExistsException{
+        Optional <Cliente> cliente =clienteDao.findByEmail(email);
+        return cliente.orElseThrow(() -> new EmailNoExistsException("Email no encontrado"));
     }
     
 }
