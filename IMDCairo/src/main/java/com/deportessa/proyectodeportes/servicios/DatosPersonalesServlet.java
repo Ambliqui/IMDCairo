64db645478597a64c8d799f5b987e2811c5e15c8
Ambliqui <isiseren@gmail.com>
2021-11-07 10:53:49 +0100

    X

--- IMDCairo/src/main/java/com/deportessa/proyectodeportes/servicios/DatosPersonalesServlet.java
+++ IMDCairo/src/main/java/com/deportessa/proyectodeportes/servicios/DatosPersonalesServlet.java
@@ -34,10 +34,11 @@ public class DatosPersonalesServlet implements ActionController {
 
         switch (metodoPago) {
             case "tarjeta":
-//                validaciones.longitudCampo("Numero de Tarjeta: ", request.getParameter("numeroTarjeta"), 4).ifPresent((error) -> exceptions.add(error));
-//                validaciones.longitudCampo("Mes Tarjeta: ", request.getParameter("mesTarjeta"), 4).ifPresent((error) -> exceptions.add(error));
-//                validaciones.longitudCampo("Año Tarjeta: ", request.getParameter("annoTarjeta"), 4).ifPresent((error) -> exceptions.add(error));
-//                validaciones.longitudCampo("CVS: ", request.getParameter("cvsTarjeta"), 4).ifPresent((error) -> exceptions.add(error));
+                validaciones.longitudCampo("Numero de Tarjeta: ", request.getParameter("numeroTarjeta"), 4).ifPresent((error) -> exceptions.add(error));
+                validaciones.rangoValores(Integer.parseInt(request.getParameter("mesTarjeta")), 1L, 12L);
+                validaciones.longitudCampo("Mes Tarjeta: ", request.getParameter("mesTarjeta"), 4).ifPresent((error) -> exceptions.add(error));
+                validaciones.longitudCampo("Año Tarjeta: ", request.getParameter("annoTarjeta"), 4).ifPresent((error) -> exceptions.add(error));
+                validaciones.longitudCampo("CVS: ", request.getParameter("cvsTarjeta"), 4).ifPresent((error) -> exceptions.add(error));
                 break;
             case "paypal":
 //                validaciones.emailNoFormateado(request.getParameter("cuentaPaypal")).ifPresent((error) -> exceptions.add(error));
@@ -57,21 +58,21 @@ public class DatosPersonalesServlet implements ActionController {
             request.setAttribute("apellidos", request.getParameter("apellidos"));
             request.setAttribute("telefono", request.getParameter("telefono"));
             
-//            switch (metodoPago) {
-//                case "tarjeta":
-//                    request.setAttribute("numeroTarjeta", request.getParameter("numeroTarjeta"));
-//                    request.setAttribute("mesTarjeta", request.getParameter("mesTarjeta"));
-//                    request.setAttribute("annoTarjeta", request.getParameter("annoTarjeta"));
-//                    request.setAttribute("cvsTarjeta", request.getParameter("cvsTarjeta"));
-//                    break;
+            switch (metodoPago) {
+                case "tarjeta":
+                    request.setAttribute("numeroTarjeta", request.getParameter("numeroTarjeta"));
+                    request.setAttribute("mesTarjeta", request.getParameter("mesTarjeta"));
+                    request.setAttribute("annoTarjeta", request.getParameter("annoTarjeta"));
+                    request.setAttribute("cvsTarjeta", request.getParameter("cvsTarjeta"));
+                    break;
 //                case "paypal":
 //                    request.setAttribute("cuentaPaypal", request.getParameter("cuentaPaypal"));
 //                    break;
 //                case "transferencia":
 //                    request.setAttribute("IBAN", request.getParameter("IBAN"));
 //                    break;
-//                default:
-//            }
+                default:
+            }
 
             //Devolvemos los errores
             request.setAttribute("errores", exceptions);
