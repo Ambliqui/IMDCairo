ec3b1201decbf6ae8d6dbfbd45b856e14b68d24d
AntonioJ93 <86188263+AntonioJ93@users.noreply.github.com>
2021-11-15 00:48:34 +0100

    Update PostMetodosPagoServlet.java

--- IMDCairo/src/main/java/com/deportessa/proyectodeportes/servlets/metodosPago/PostMetodosPagoServlet.java
+++ IMDCairo/src/main/java/com/deportessa/proyectodeportes/servlets/metodosPago/PostMetodosPagoServlet.java
@@ -30,18 +30,60 @@ public class PostMetodosPagoServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet PostMetodosPagoServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet PostMetodosPagoServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+         ////////////// DATOS DE USUARIO //////////////////////
+        
+        DatosLoginVO datosLogin = (DatosLoginVO) request.getAttribute("datosLogin");
+        DatosPersonalesVO datosCliente = (DatosPersonalesVO) request.getAttribute("datosCliente");
+
+        Cliente cliente = new Cliente();
+        cliente.setEmailCliente(datosLogin.getEmailCliente());
+        cliente.setPassCliente(datosLogin.getPassCliente());
+        cliente.setNombreCliente(datosCliente.getNombreCliente());
+        cliente.setApellido1Cliente(datosCliente.getApellido1Cliente());
+        cliente.setTelefonoCliente(datosCliente.getTelefonoCliente());
+        daoFactoryLocal.getClienteDaoLocal().create(cliente);
+
+        MetodoPago metodoPago;
+        Tarjeta tarjeta = null;
+        Paypal paypal = null;
+        Transferencia transferencia = null;
+
+        if (request.getParameter("metodoPago").equalsIgnoreCase("tarjeta")) {
+
+            Integer numeroTarjeta = Integer.parseInt(request.getParameter("numeroTarjeta"));
+            Integer mesTarjeta = Integer.parseInt(request.getParameter("mesTarjeta"));
+            Integer annoTarjeta = Integer.parseInt(request.getParameter("annoTarjeta"));
+            Integer csvTarjeta = Integer.parseInt(request.getParameter("cvsTarjeta"));
+            tarjeta = new Tarjeta(numeroTarjeta, mesTarjeta, annoTarjeta, csvTarjeta);
+            daoFactoryLocal.getTarjetaDaoLocal().create(tarjeta);
+
+            cliente = daoFactoryLocal.getClienteDaoLocal().findByEmail(cliente.getEmailCliente()).get();
+            cliente.addMPago(tarjeta);
+            daoFactoryLocal.getClienteDaoLocal().edit(cliente);
+
+        } else if (request.getParameter("metodoPago").equalsIgnoreCase("paypal")) {
+
+            paypal = new Paypal(request.getParameter("cuentaPaypal"));
+            cliente = daoFactoryLocal.getClienteDaoLocal().findByEmail(cliente.getEmailCliente()).get();
+            daoFactoryLocal.getPayPalDaoLocal().create(paypal);
+
+            cliente.addMPago(paypal);
+            daoFactoryLocal.getClienteDaoLocal().edit(cliente);
+
+        } else {
+
+            transferencia = new Transferencia(Integer.parseInt(request.getParameter("IBAN")));
+            cliente = daoFactoryLocal.getClienteDaoLocal().findByEmail(cliente.getEmailCliente()).get();
+            daoFactoryLocal.getTransferenciaDaoLocal().create(transferencia);
+
+            cliente.addMPago(transferencia);
+            daoFactoryLocal.getClienteDaoLocal().edit(cliente);
+
+        }
+
+        request.getSession(true);
+        request.getSession().setAttribute("clienteSession", cliente);
+        request.getRequestDispatcher("PrePrincipalServlet").forward(request, response);
         }
     }
 
