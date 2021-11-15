6005d5756f559dfd7ff869f37a12214ffe38a423
AntonioJ93 <86188263+AntonioJ93@users.noreply.github.com>
2021-11-15 01:21:36 +0100

    +

--- IMDCairo/src/main/java/com/deportessa/proyectodeportes/servlets/metodosPago/PostMetodosPagoServlet.java
+++ IMDCairo/src/main/java/com/deportessa/proyectodeportes/servlets/metodosPago/PostMetodosPagoServlet.java
@@ -4,8 +4,16 @@
  */
 package com.deportessa.proyectodeportes.servlets.metodosPago;
 
+import com.deportessa.proyectodeportes.daojpa.factory.DaoAbstractFactoryLocal;
+import com.deportessa.proyectodeportes.modelo.Cliente;
+import com.deportessa.proyectodeportes.modelo.MetodoPago;
+import com.deportessa.proyectodeportes.modelo.Paypal;
+import com.deportessa.proyectodeportes.modelo.Tarjeta;
+import com.deportessa.proyectodeportes.modelo.Transferencia;
+import com.deportessa.proyectodeportes.servicios.dto.DatosLoginVO;
+import com.deportessa.proyectodeportes.servicios.dto.DatosPersonalesVO;
 import java.io.IOException;
-import java.io.PrintWriter;
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -28,14 +36,67 @@ public class PostMetodosPagoServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    
+    @Inject
+    private DaoAbstractFactoryLocal daoFactoryLocal;
+    
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+         ////////////// DATOS DE USUARIO //////////////////////
         
-        
-        
-        
-        
-        
+        DatosLoginVO datosLogin = (DatosLoginVO) request.getAttribute("datosLogin");
+        DatosPersonalesVO datosCliente = (DatosPersonalesVO) request.getAttribute("datosCliente");
+
+        Cliente cliente = new Cliente();
+        cliente.setEmailCliente(datosLogin.getEmailCliente());
+        cliente.setPassCliente(datosLogin.getPassCliente());
+        cliente.setNombreCliente(datosCliente.getNombreCliente());
+        cliente.setApellido1Cliente(datosCliente.getApellido1Cliente());
+        cliente.setTelefonoCliente(datosCliente.getTelefonoCliente());
+        daoFactoryLocal.getClienteDaoLocal().create(cliente);
+
+        MetodoPago metodoPago;
+        Tarjeta tarjeta = null;
+        Paypal paypal = null;
+        Transferencia transferencia = null;
+
+        if (request.getParameter("metodoPago").equalsIgnoreCase("tarjeta")) {
+
+            Integer numeroTarjeta = Integer.parseInt(request.getParameter("numeroTarjeta"));
+            Integer mesTarjeta = Integer.parseInt(request.getParameter("mesTarjeta"));
+            Integer annoTarjeta = Integer.parseInt(request.getParameter("annoTarjeta"));
+            Integer csvTarjeta = Integer.parseInt(request.getParameter("cvsTarjeta"));
+            tarjeta = new Tarjeta(numeroTarjeta, mesTarjeta, annoTarjeta, csvTarjeta);
+            daoFactoryLocal.getTarjetaDaoLocal().create(tarjeta);
+
+            cliente = daoFactoryLocal.getClienteDaoLocal().findByEmail(cliente.getEmailCliente()).get();
+            cliente.addMPago(tarjeta);
+            daoFactoryLocal.getClienteDaoLocal().edit(cliente);
+
+        } else if (request.getParameter("metodoPago").equalsIgnoreCase("paypal")) {
+
+            paypal = new Paypal(request.getParameter("cuentaPaypal"));
+            cliente = daoFactoryLocal.getClienteDaoLocal().findByEmail(cliente.getEmailCliente()).get();
+            daoFactoryLocal.getPayPalDaoLocal().create(paypal);
+
+            cliente.addMPago(paypal);
+            daoFactoryLocal.getClienteDaoLocal().edit(cliente);
+
+        } else {
+
+            transferencia = new Transferencia(Integer.parseInt(request.getParameter("IBAN")));
+            cliente = daoFactoryLocal.getClienteDaoLocal().findByEmail(cliente.getEmailCliente()).get();
+            daoFactoryLocal.getTransferenciaDaoLocal().create(transferencia);
+
+            cliente.addMPago(transferencia);
+            daoFactoryLocal.getClienteDaoLocal().edit(cliente);
+
+        }
+
+        request.getSession(true);
+        request.getSession().setAttribute("clienteSession", cliente);
+        request.getRequestDispatcher("PrePrincipalServlet").forward(request, response);
         
     }
 
